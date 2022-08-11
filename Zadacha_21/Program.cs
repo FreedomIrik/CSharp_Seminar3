9519073a088e4b69325dcf6b2a97d5ca8bd105ae
Irik <freedomirik@yandex.ru>
2022-08-11 23:10:29 +0500

    Задача №21

---
+++ Zadacha_21/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 2D пространстве.*/
+
+System.Console.WriteLine("Введите координаты точки А по оси x: ");
+int Ax = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите координаты точки А по оси y: ");
+int Ay = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите координаты точки B по оси x: ");
+int Bx = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите координаты точки B по оси y: ");
+int By = Convert.ToInt32(System.Console.ReadLine());
+
+double distanceAB = Math.Sqrt((Bx-Ax)*(Bx-Ax)+(By-Ay)*(By-Ay));
+
+System.Console.WriteLine("Длина отрезка АВ: " + distanceAB);
