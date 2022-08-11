ddc12f68e7f6a675f9f3d6c310a1f9636e12c1c4
Irik <freedomirik@yandex.ru>
2022-08-11 23:24:02 +0500

    Задача №22

---
+++ Zadacha_22/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 22: Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу квадратов чисел от 1 до N.*/
+
+System.Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(System.Console.ReadLine());
+
+int index = 1;
+
+while (index <= numberN)
+{
+  int sqr = index * index;
+    System.Console.Write(" " + sqr);
+    index++;
+}
